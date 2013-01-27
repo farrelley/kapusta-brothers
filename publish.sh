9bdb493969a46c677d577cb692abb5078ba98828
Shaun Farrell <farrelley@gmail.com>
2013-01-27 09:06:25 -0500

    Modified publish script

--- publish.sh
+++ publish.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+rm -rf output_prod
 
 vendor/bin/sculpin generate --env=prod || ( echo "Could not generate the site" && exit )
 vendor/bin/sculpin assets:install --env=prod output_prod || ( echo "Could not install assets for the site" && exit )
-rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html || ( echo "Could not publish the site" && exit )
+
+cd output_prod
+git init
+git add .
+git add -u
+git commit -m "New Build"
+git remote add origin git@github.com:farrelley/kapusta-brothers.git
+git fetch origin
+git checkout gh-pages
+git push origin gh-pages --force
