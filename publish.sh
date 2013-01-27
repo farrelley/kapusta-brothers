c6d8546fe161598c148d07d89fe3d5cec726c7a9
Shaun Farrell <farrelley@gmail.com>
2013-01-27 10:08:15 -0500

    Updated script to push to gh-pages

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 rm -rf output_prod
 
 vendor/bin/sculpin generate --env=prod || ( echo "Could not generate the site" && exit )
@@ -7,9 +8,7 @@ vendor/bin/sculpin assets:install --env=prod output_prod || ( echo "Could not in
 cd output_prod
 git init
 git add .
-git add -u
 git commit -m "New Build"
+git branch -m gh-pages
 git remote add origin git@github.com:farrelley/kapusta-brothers.git
-git fetch origin
-git checkout gh-pages
 git push origin gh-pages --force
